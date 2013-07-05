5ece1826ca27c57dbd6488c0896874571c4b6e5c
Abraham Godong <abe@godong.or.id>
2013-07-05 07:50:49 -0700

    Add Left and Right Button, navigation. Move CSS to a CSS file.

--- src/js/jquery.dataslider.js
+++ src/js/jquery.dataslider.js
@@ -27,7 +27,7 @@
             slideLeft: function(from, to) {
                 from.css( { 'display' : "block", 'position': "absolute", 'top': 0, 'left': 0, width: slider.width, height: slider.height, 'z-index' : 10 });
                 to.css( { 'display' : "block", 'position': "absolute", 'top': 0, 'left': slider.width, width: slider.width, height: slider.height, 'z-index' : 20 });
-                slider.root.animate({ scrollLeft: to.position().left }, 'slow', 'easeInOutExpo', function() {
+                slider.wrap.animate({ scrollLeft: to.position().left }, 'slow', 'easeInOutExpo', function() {
                     slider.animations.showHide(from, to);
                 });
             },
@@ -58,27 +58,50 @@
             slider = $.extend( {
                 root: root,
                 slides: slides,
+                wrap: slides.wrapAll('<div class="slider-wrap" />').parent(),
                 slidesTotal: slides.length,
                 width: $(root).width(),
-                height: $(root).height()
+                height: $(root).height(),
+                buttonLeft: $('<div />').addClass('button button-left').appendTo(root),
+                buttonRight: $('<div />').addClass('button button-right').appendTo(root),
+                nav: $('<ul />').addClass('nav-wrap').appendTo(root)
             }, slider);
-            slider.root.css( { 'position': "relative", 'overflow': "hidden", 'width': slider.width, 'height': slider.height } );
+            slider.root.addClass('slider').css( { 'position': "relative", 'overflow': "hidden", 'width': slider.width, 'height': slider.height } );
+            slider.wrap.css( { 'position': "relative", 'overflow': "hidden", 'width': slider.width, 'height': slider.height } );
+            slider.buttonLeft.click(function() {
+                var from = slider.root.data("current");
+                //console.log(from);
+                slider.rotate(from, slider.findIndex(from-1));
+            });
+            slider.buttonRight.click(function() {
+                var from = slider.root.data("current");
+                slider.rotate(from, slider.findIndex(from+1));
+            });
+            //Handle button
+            if (slider.root.data("options").useButtons === "hover" || slider.root.data("options").useButtons === true) {
+                slider.root.hover(function() { slider.showButton(); }, function() { slider.hideButton(); });
+            }
+            else if (slider.root.data("options").useButtons === "always") {
+                slider.showButton();
+            }
+            slider.buildNavs();
             slider.rotate(0, 0);
-
+            //Handle navs
         },
         rotate: function(from, to) {
             //hide previous
             //show current
+            window.clearTimeout(slider.root.data("timer"));
             slider.root.data("current",to);
             //Apply animations
             var data = $(slider.slides[to]).data();
-            var transition = (data.transition === undefined)? $.data(root, "defaultTransition"):data.transition;
+            var transition = (data.transition === undefined)? $.data(slider.root, "defaultTransition"):data.transition;
             slider.animations[transition]($(slider.slides[from]), $(slider.slides[to]));
             from = to;
             to = slider.findIndex(to + 1);
             data = $(slider.slides[to]).data();
             //if (loop || cursor !== 0) {
-            slider.root.data("timer", window.setTimeout(function() { slider.rotate(from, to) }, data.timing ));
+                slider.root.data("timer", window.setTimeout(function() { slider.rotate(from, to) }, data.timing ));
             //}
         },
         findIndex: function(cursor) {
@@ -87,16 +110,34 @@
         },
         addAnimation: function(funct) {
             slider.animations = $.extend(funct, slider.animations);
+        },
+        showButton: function() {
+            slider.buttonLeft.show();
+            slider.buttonRight.show();
+            //Must check show button is enabled
+        },
+        hideButton: function() {
+            slider.buttonLeft.hide();
+            slider.buttonRight.hide();
+        },
+        buildNavs: function() {
+            $.each(slider.slides, function(key, value) {
+                $('<li />').addClass('nav').appendTo(slider.nav).html(key+1).click(function() {
+                    slider.rotate(slider.root.data("current"), key);
+                });
+            });
         }
     }
     $.fn.dataslide = function(options) {
         var options = $.extend( {
             useTitle: true,
-            useButtons: true,
+            useButtons: "hover",
+            useNavs: true,
             defaultTransition: "none",
             autoPlay: true,
             loop: true,
-            rememberLocation: false
+            rememberLocation: false,
+            hoverPause: false
         }, options);
         this.data("options", options);
         $(this).children().css( { 'display': "none" });
